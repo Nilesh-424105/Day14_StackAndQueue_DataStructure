4955613dc8aa6272642a9ca2b8136a5458e8b54e
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-28 15:15:25 +0530

    ADDED: Welcome Message

---
+++ com/datastructure/MyStackTest.java
@@ -0,0 +1,9 @@
+package com.datastructure;
+
+public class MyStackTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Stack And Queue Data Structure Program");
+	}
+}
